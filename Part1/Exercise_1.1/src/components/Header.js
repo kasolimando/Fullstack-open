0be5d621178e15d8256be7f1bbdb4a6bb47f01de
kasolimando <kasolimando@gmail.com>
2024-02-18 14:50:55 -0400

    Part 1

---
+++ Part1/Exercise_1.1/src/components/Header.js
@@ -0,0 +1,8 @@
+const Header  = (props) => {
+  return (
+    <>
+     <h1>{props.course}</h1>
+    </>
+  )
+}
+export default Header;
---
+++ Part1/Exercise_1.3-1.5/src/components/Total.js
@@ -0,0 +1,9 @@
+const Total  = (props) => {
+  let parts = props.course.parts;
+  return (
+    <>
+     <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </>
+  )
+} 
+export default Total;
